dd09ff074e771f8d8dd68318d4546cad9d6d0c33
Riccardo Rao <riccardo.rao.c@gmail.com>
2019-07-19 21:19:03 -0400

    replication and mutation multi-threaded

--- simplest.jl
+++ simplest.jl
@@ -1,24 +1,33 @@
 
-using Random, Distances, Plots
+using Random, Distances, Plots, Base.Threads
+import Future
 
-const NGEN, NPOP, DGTY, REPRATE, MUTRATE  = 100000, 10000, 5, 1.0, 0.1
+const NGEN, NPOP, DGTY = 1000, 1000, 2
+const REPRATE, MUTRATE = 10., 0.5
+const FITNESSOFFSET, DELTATOFFSET =  1.0, 1.0
 
 # fitness function
-fitness(gty::Array{Float64,1}, Dgty::Int64)::Float64 = 1/(euclidean(gty,ones(Float64,Dgty))+.1)
+fitness(gty::Array{Float64,1}, Dgty::Int64)::Float64 = 1/(euclidean(gty,ones(Float64,Dgty))+FITNESSOFFSET)
 
 # replication function: ( population of genotypes, initial population size, dimension genotypic space ) → replicated population
-function replication!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float64,1}, pNpopGty::Array{Int64,1}, repFactor::Float64)
-	for i in 1:pNpopGty[2]
+function replication!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float64,1}, pNpopGty::Array{Int64,1}, repFactor::Float64, R::Vector{MersenneTwister})
+	G = zeros(Int64,pNpopGty[2])
+	@threads for i in 1:pNpopGty[2]
 		Kr::Float64 = repFactor*fitPopGty[i]
 		ipKr::Int8 = trunc(Int32,Kr)
 
-		G::Int8 = rand() < Kr - ipKr ? ipKr + 1 : ipKr
+		G[i] = rand(R[threadid()]) < Kr - ipKr ? ipKr + 1 : ipKr
+		# G::Int64 = rand() < Kr - ipKr ? ipKr + 1 : ipKr
+	end
 
-		for inew in 1:G
+	for i in 1:pNpopGty[2]
+		for inew in 1:G[i]
 			pNpopGty[1] += 1
-			if pNpopGty[1] <= length(popGty)
+			if pNpopGty[1] <= pNpopGty[3]
 				popGty[pNpopGty[1]] = deepcopy(popGty[i])
+				fitPopGty[pNpopGty[1]] = fitPopGty[i]
 			else
+				pNpopGty[3] += 1
 				push!(popGty,popGty[i])
 				push!(fitPopGty,fitPopGty[i])
 			end
@@ -26,12 +35,12 @@ function replication!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float6
 	end
 end
 
-# mutation function: ( genotype, dimension genotypic space ) → mutated genotype
-function effMutation!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float64,1}, pNpopGty::Array{Int64,1}, Dgty::Int64, repFactor::Float64, mutProb::Float64)
+# mutation function: ( genotype, fitness, dimension genotypic space ) → mutated genotype
+function effMutation!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float64,1}, pNpopGty::Array{Int64,1}, Dgty::Int64, repFactor::Float64, mutProb::Float64, R::Vector{MersenneTwister})
 	Δx::Float32 = .01
 
-	for i in 1:pNpopGty[1]
-		r1::Float32 = rand()
+	@threads for i in 1:pNpopGty[1]
+		r1::Float32 = rand(R[threadid()])
 		pCum::Float32 = 0.
 		xvar::Int16 = -1
 
@@ -48,15 +57,21 @@ function effMutation!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float6
 	end
 end
 
-function effSelection!(popGty::Array{Array{Float64,1},1}, pNpopGty::Array{Int64,1})
-
+# effective selection function: ( genotype populations, fitness, population size ) → selected population
+function effSelection!(popGty::Array{Array{Float64,1},1}, fitPopGty::Array{Float64,1}, pNpopGty::Array{Int64,1}; ubermode::Bool=false)
 	growthFactor::Float64 = log(pNpopGty[1]/pNpopGty[2])
-	popGtyRef = copy(popGty)
+	popGtyRef, fitPopGtyRef = copy(popGty), copy(fitPopGty)
 
-	# selection with replacement of individuals
-	survivedGty = rand(collect(1:pNpopGty[1]),pNpopGty[2])
+	if ubermode
+		# survival of the fittest
+		survivedGty = sortperm(fitPopGty,rev=true)
+	else
+		# selection with replacement of individuals
+		survivedGty = rand(collect(1:pNpopGty[1]),pNpopGty[2])
+	end
 	for i in 1:pNpopGty[2]
 		popGty[i] = popGtyRef[survivedGty[i]]
+		fitPopGty[i] = fitPopGtyRef[survivedGty[i]]
 	end
 
 	# population size renormalization
@@ -65,45 +80,50 @@ function effSelection!(popGty::Array{Array{Float64,1},1}, pNpopGty::Array{Int64,
 	return growthFactor
 end
 
+# ==============================
+
 # main function
 function main()#::Int8
-	# variable Npopulation as a "pointer"
-	pNpop = Int64[NPOP,NPOP]
+	R = let m = MersenneTwister(1)
+	        [m; accumulate(Future.randjump, fill(big(10)^20, nthreads()-1), init=m)]
+	    end;
+
+	# variable Npopulation as a "pointer" : effective population, fixed population value, array size
+	pNpop = Int64[NPOP,NPOP,3*NPOP]
 
 	# ∀ individual → genotypic variable
 	# the factor 2 accounts for the maximum population growth in one step
-	𝕏 = [ zeros(Float64,DGTY) for i in 1:2*pNpop[1] ]
+	𝕏 = [ zeros(Float64,DGTY) for i in 1:pNpop[3] ]
 
 	# ∀ individual → fitness value
-	𝔽 = Float64[ i <= pNpop[1] ? fitness(𝕏[i],DGTY) : 0.0 for i in 1:2*pNpop[1] ]
+	𝔽 = Float64[ i <= pNpop[1] ? fitness(𝕏[i],DGTY) : 0.0 for i in 1:pNpop[3] ]
 
-	# growth factors
+	# growth factors and average fitness
 	μ = zeros(Float64,NGEN)
+	ϕ = zeros(Float64,NGEN)
 
-	println("\n", 𝕏[1:pNpop[1]])
+	# println("\n", 𝕏[1:pNpop[1]])
 	# println("\n", 𝕏, " is a: ", typeof(𝕏))
 	# println(𝔽, " is a: ", typeof(𝔽))
 
 
 	# evolution time scale, mutation probability, and replication factor
-	Δt::Float64 = 1/(2*DGTY*MUTRATE+.01)
+	Δt::Float64 = 1/(2*DGTY*MUTRATE+DELTATOFFSET)
 	κm::Float64 = MUTRATE*Δt
 	κr::Float64 = REPRATE*Δt
 
 	for gen in 1:NGEN
-		replication!(𝕏,𝔽,pNpop,κr)
-		effMutation!(𝕏,𝔽,pNpop,DGTY,κr,κm)
-		μ[gen] = effSelection!(𝕏,pNpop)
+		replication!(𝕏,𝔽,pNpop,κr,R)
+		effMutation!(𝕏,𝔽,pNpop,DGTY,κr,κm,R)
+		μ[gen] = effSelection!(𝕏,𝔽,pNpop,ubermode=true)
+		ϕ[gen] = sum(𝔽[1:pNpop[2]])/pNpop[2]
 	end
 
-	println(𝕏[1:pNpop[1]])
+	# println(𝕏[1:pNpop[1]])
 
-	pl = plot(collect(1:NGEN),μ)
-
-	return μ
+	pyplot() # Switch to using the PyPlot.jl backend
+	return plot(collect(1:NGEN),ϕ)
+	# return plot(collect(1:NGEN),μ)
 end
 
-mu = main()
-
-pyplot() # Switch to using the PyPlot.jl backend
-plot(collect(1:NGEN),mu)
+# main()
