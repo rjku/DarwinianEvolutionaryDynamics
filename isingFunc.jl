bd6529c9b1e5de91f22f45d0aad5ee09e2763568
Riccardo Rao <riccardo.rao.c@gamil.com>
2019-08-14 23:06:42 +0200

    ising2d. typified system and methodized simulations

--- isingFunc.jl
+++ isingFunc.jl
@@ -5,79 +5,96 @@ SYSTEMSIZE = 30
 
 # the (initial) state vector
 # n = rand([-1,1], 	L, L)
+n = ones(Int8, SYSTEMSIZE, SYSTEMSIZE)
 
 # the (initial) interaction matrix
 # interactionmatrix = ones(Float32,2*SYSTEMSIZE^2)
 interactionmatrix = rand(Float32,2*SYSTEMSIZE^2)
 
-# ising 2d function simulator ( state n, system size L, interaction matrix Jij, input field h) → readout magnetization m
-function ising2d(L::Integer, Jij::Array{T,1}, hi::AbstractFloat)::AbstractFloat where {T<:AbstractFloat}
-	n = ones(Int16, L, L)
+# type: ising signal transduction
+struct tIsingST
+	L::Int32				# system size
+	β::Float32 				# inverse temperature [ critical inverse temperature ≃ 1/2.3 ≃ 0.43 ]
+	he::Float32 			# global external field H
 
-	L2::Int16 = L^2;	halfL::Int16 = L÷2
+	L2::Int32;	halfL::Int32
+	li::Int32;	li2::Int32			# input/redout region size set to one tenth of the system size
+	ℍe::Float32
 
-	β::Float32 = 0.7 			# critical inverse temperature ≃ 1/2.3 ≃ 0.43
-	he::Float32 = 0.5 			# global external field H
-	Nmcs::Int32 =  50*L2		# number of Monte Carlo steps
-	Nsamplings::Int16 = 20		# number of samplings points
+	# definition: useful nearest neighbour coordinate vectors
+	jp::Array{Int32,1}; jm::Array{Int32,1}
+	Jpi::Array{Int32,2}; Jmi::Array{Int32,2}; Jpj::Array{Int32,2}; Jmj::Array{Int32,2}
+end
 
-	li::Int16 = L÷10;	li2::Int16 = li^2			# input/redout region size set to one tenth of the system size
+# constructor: ising signal transduction
+tIsingST(L, β, he) = tIsingST(
+	L, β, he, L^2, L÷2, L÷10, (L÷10)^2, he*β,
+	Int32[i%L+1 for i in 1:L],
+	Int32[(L-2+i)%L+1 for i in 1:L],
+	Int32[i+2(j-1)*L for i in 1:L, j in 1:L],
+	Int32[(L-2+i)%L+1+2(j-1)*L for i in 1:L, j in 1:L],
+	Int32[i+(2j-1)*L for i in 1:L, j in 1:L],
+	Int32[i+(2(L-2+j)%L+1)*L for i in 1:L, j in 1:L]
+)
 
-	𝕁::Array{Float32,1} = Jij*β;	ℍe::Float32 = he*β;		ℍi::Float32 = hi*β
+
+# MonteCarlo simulation function for ising signal transduction
+# 	( tIsingST isingST, state n, interaction matrix Jij, input field h ) → readout magnetization m
+function monteCarlo(isingST::tIsingST, n::Array{Int8,2}, Jij::Array{T,1}, hi::AbstractFloat) where {T<:AbstractFloat}
+	Nmcs::Int32 =  50*isingST.L2		# number of Monte Carlo steps
+	Nsamplings::Int16 = 20				# number of samplings points
+	𝕁::Array{Float32,1} = Jij*isingST.β;	ℍi::Float32 = hi*isingST.β
 
 	# definition: time-averaged readout magnetization, time-averaged spin config, and overall magnetization
 	mro::Float32 = 0.0
-	# aves = zeros(Int16, L, L)
+	aves = zeros(Float32, isingST.L, isingST.L)
 	# m = zeros(Float32, Nsamplings)
-
-	# definition: useful nearest neighbour coordinate vectors
-	jp = collect(Int16, 2:L);	sizehint!(jp,L); 	jp = push!(jp,1)
-	jm = collect(Int16, 1:L-1);	sizehint!(jm,L); 	jm = pushfirst!(jm,L)
-
-	Jpi = Int16[i+2(j-1)*L for i in 1:L, j in 1:L]
-	Jmi = Int16[jm[i]+2(j-1)*L for i in 1:L, j in 1:L]
-	Jpj = Int16[i+(2j-1)*L for i in 1:L, j in 1:L]
-	Jmj = Int16[i+(2*jm[j]-1)*L for i in 1:L, j in 1:L]
 
 	for is in 1:Nsamplings
 		for imcs in 1:Nmcs
 			# Monte Carlo step evaluated using Glauber transition rates:
 			# next possibly transitioning spin (coordinates)
-			i, j = rand(collect(1:L)), rand(collect(1:L))
+			i, j = rand(collect(1:isingST.L)), rand(collect(1:isingST.L))
 			# transitioning?!
-			if i <= li && j <= li
-				if rand() < ( 1. - n[i,j]*tanh( 𝕁[Jpi[i,j]]*n[jp[i],j] + 𝕁[Jmi[i,j]]*n[jm[i],j] +
-						𝕁[Jpj[i,j]]*n[i,jp[j]] + 𝕁[Jmj[i,j]]*n[i,jm[j]] + ℍe + ℍi ))/2
+			if i <= isingST.li && j <= isingST.li
+				if rand() < ( 1. - n[i,j]*tanh(
+						𝕁[isingST.Jpi[i,j]]*n[isingST.jp[i],j] + 𝕁[isingST.Jmi[i,j]]*n[isingST.jm[i],j] +
+						𝕁[isingST.Jpj[i,j]]*n[i,isingST.jp[j]] + 𝕁[isingST.Jmj[i,j]]*n[i,isingST.jm[j]] +
+						isingST.ℍe + ℍi ))/2
 					n[i,j] = - n[i,j]
 				end
 			else
-				if rand() < ( 1. - n[i,j]*tanh( 𝕁[Jpi[i,j]]*n[jp[i],j] + 𝕁[Jmi[i,j]]*n[jm[i],j] +
-						𝕁[Jpj[i,j]]*n[i,jp[j]] + 𝕁[Jmj[i,j]]*n[i,jm[j]] + ℍe ))/2
+				if rand() < ( 1. - n[i,j]*tanh(
+						𝕁[isingST.Jpi[i,j]]*n[isingST.jp[i],j] + 𝕁[isingST.Jmi[i,j]]*n[isingST.jm[i],j] +
+						𝕁[isingST.Jpj[i,j]]*n[i,isingST.jp[j]] + 𝕁[isingST.Jmj[i,j]]*n[i,isingST.jm[j]] +
+						isingST.ℍe ))/2
 					n[i,j] = - n[i,j]
 				end
 			end
 		end
 
 		# evaluation: time-averaged readout magnetization, time-averaged spin config, and overall magnetization
-		mro += sum(n[halfL+1:halfL+li,halfL+1:halfL+li])
-		# aves += n
-		# m[is] = sum(n)/L2
+		mro += sum(n[isingST.halfL+1:isingST.halfL+isingST.li,isingST.halfL+1:isingST.halfL+isingST.li])
+		aves += n
+		# m[is] = sum(n)/isingST.L2
 	end
 
-	# aves /= Nsamplings
-	mro /= Nsamplings * li2
+	aves /= Nsamplings
+	mro /= Nsamplings * isingST.li2
 
 	# println(n)
 
 	# pyplot() # Switch to using the PyPlot.jl backend
 	# return plot(collect(1:Nsamplings), m)
-	# matshow(aves,cmap="Greys_r"); gcf()
+	matshow(aves,cmap="Greys_r"); gcf()
 	# matshow(n,cmap="Greys_r"); gcf()
 
-	return mro
+	# return mro
 end
 
-@time println(ising2d(SYSTEMSIZE,interactionmatrix,-10.0))
-# @time ising2d(SYSTEMSIZE,interactionmatrix,-10.0)
+isingST = tIsingST(SYSTEMSIZE,0.7,0.5)
+
+# @time println(monteCarlo(isingST,n,interactionmatrix,-10.0))
+@time monteCarlo(isingST,n,interactionmatrix,-10.0)
 
 # Juno.@run ising2d(SYSTEMSIZE,interactionmatrix)
